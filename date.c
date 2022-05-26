01f71d0692426a8fa9fcbf6480260379eed09139
sabdulmajid <ayman.hasib@outlook.com>
2022-05-26 23:44:43 +0300

    Converting numerical to written date - complete

--- date.c
+++ date.c
@@ -14,50 +14,70 @@ int main(void)
     switch (month)
     {
     case 1:
-        printf("January\n");
+        printf("January");
         break;
     case 2:
-        printf("February\n");
+        printf("February");
         break;
     case 3:
-        printf("March\n");
+        printf("March");
         break;
     case 4:
-        printf("April\n");
+        printf("April");
         break;
     case 5:
-        printf("May\n");
+        printf("May");
         break;
     case 6:
-        printf("June\n");
+        printf("June");
         break;
     case 7:
-        printf("July\n");
+        printf("July");
         break;
     case 8:
-        printf("August\n");
+        printf("August");
         break;
     case 9:
-        printf("September\n");
+        printf("September");
         break;
     case 10:
-        printf("October\n");
+        printf("October");
         break;
     case 11:
-        printf("November\n");
+        printf("November");
         break;
     case 12:
-        printf("December\n");
+        printf("December");
         break;
     default:
-        printf("Invalid month!\n");
+        printf("Invalid month!");
     }
 
     // Print the day
+    printf(" %d", day);
 
     // Print the suffix for a given day
+    switch (day)
+    {
+    case 1:
+    case 21:
+    case 31:
+        printf("st");
+        break;
+    case 2:
+    case 22:
+        printf("nd");
+        break;
+    case 3:
+    case 23:
+        printf("rd");
+        break;
+    default:
+        printf("th");
+    }
 
     // Print the year
+    printf(", %d.\n", year);
 
     return 0;
 }
