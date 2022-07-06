d38e8b40faf2f07e42dec0bf65fe659bd1f663c4
sabdulmajid <ayman.hasib@outlook.com>
2022-07-06 04:08:14 -0400

    Fixed broken converter program

--- temperatureConverter.c
+++ temperatureConverter.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
-#include <stdbool.h>
+#define FREEZING_POINT 32.0f
+#define SCALE_FACTOR (5.0f / 9.0f)
+
 
 int main (void) {
-    bool unit;
-    char* units;
-    scanf("%s", units);
+    float faren, celc;
+    printf("Enter the Farenheit temperature: ");
+    scanf("%f", &faren);
+
+    celc = (faren - FREEZING_POINT) * SCALE_FACTOR;
+
+    printf("Celsius temperature: %.1f", celc);
 }
