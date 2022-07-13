bb275d331b9e492d8710e72beda520b862b06605
sabdulmajid <ayman.hasib@outlook.com>
2022-07-13 10:53:33 -0400

    Extensive practise with functions + rand() funct.

---
+++ repeatingRandomNumber.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void repeatDigit(int);
+int getRandomNumber(int);
+
+void repeatDigit(int repetitions) {
+  int digit = getRandomNumber(9);
+  for(int i = 0; i < repetitions; i++) {
+    printf("%d ", digit);
+  }
+  printf("\n");
+}
+
+int getRandomNumber(int maxNumber) {
+  int randomNumber = rand() % maxNumber + 1;
+  return randomNumber;
+}
+
+int main(void) {
+  srand(time(NULL));
+  int repetitions = getRandomNumber(10);
+  repeatDigit(repetitions);
+}
